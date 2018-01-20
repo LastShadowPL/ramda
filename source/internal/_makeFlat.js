b21da2ed434f3d77a288c5b37e1e1e76385fc0ff
Tomasz Surowiec <xeloxpl@windowslive.com>
2018-01-20 10:16:15 +0100

    rewritten _makeFlat in Functional ES6

--- source/internal/_makeFlat.js
+++ source/internal/_makeFlat.js
@@ -1,33 +1,21 @@
-import _isArrayLike from './_isArrayLike';
-
-
 /**
  * `_makeFlat` is a helper function that returns a one-level or fully recursive
  * function based on the flag passed in.
  *
- * @private
+ * @private 
  */
-export default function _makeFlat(recursive) {
-  return function flatt(list) {
-    var value, jlen, j;
-    var result = [];
-    var idx = 0;
-    var ilen = list.length;
-
-    while (idx < ilen) {
-      if (_isArrayLike(list[idx])) {
-        value = recursive ? flatt(list[idx]) : list[idx];
-        j = 0;
-        jlen = value.length;
-        while (j < jlen) {
-          result[result.length] = value[j];
-          j += 1;
-        }
-      } else {
-        result[result.length] = list[idx];
-      }
-      idx += 1;
-    }
-    return result;
+// Remove single line comments
+// It passes base test 
+//    _makeFlat([1,2,[[3,4], 5, 6, [7,[8, [9, 10, 11], 12]]]])
+//      => [ 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12 ]
+export default const _makeFlat = (recursive) => {
+  const isFinal = (item) => {
+    return Array.isArray(item) ? item.reduce((res, it) => !Array.isArray(it), false) : true 
   };
-}
+  
+  recursive = Array.isArray(recursive) 
+    ? recursive
+      .reduce((result, arrItem) => Array.isArray(arrItem) ? result.concat(_makeFlat(arrItem)) : result.concat(arrItem), [])
+    : recursive;
+    return isFinal(recursive) ? recursive : _makeFlat(recursive);
+};
